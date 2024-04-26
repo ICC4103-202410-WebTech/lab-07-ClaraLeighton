8c377b125fd58541bcae925cd10f31699fa9cbc4
ClaraLeighton <c.leighton@miuandes.cl>
2024-04-26 17:40:23 -0400

    Validaciones

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ApplicationRecord
 
     has_many :post_tags
     has_many :tags, :through => :post_tags
+
+    validates :title, :content, :user_id, presence: true
+    validates :answers_count, :likes_count, numericality: { greater_than_or_equal_to: 0 }
+    before_save :set_default_published_at
+    private 
+    def set_default_published_at
+      self.published_at ||= Time.current
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
+    has_many :posts 
+
+    validates :name, :email, :password, presence: true
+    validates :email, uniquenese:true
+    validates :password, lenght: { minimun: 6}
 end
